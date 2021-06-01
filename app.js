68f126e4a9dbb68ce43f75ada37a0bb05b12c739
boysers <boyser2015@gmail.com>
2021-06-01 17:16:37 +0200

    refactor compteur

--- app.js
+++ app.js
@@ -1,114 +1,129 @@
-const inputAll = document.querySelectorAll("input");
-function inputOff() {
-  for (let i = 0; i < inputAll.length; i++) {
-    inputAll[i].setAttribute("readonly", true);
-  }
+let inputDefault = {
+  min: '05',
+  sec: '00'
 }
 
-function inputOn() {
-  for (let i = 0; i < inputAll.length; i++) {
-    inputAll[i].readOnly = false;
-  }
-}
+let timerSec = 0;
 
-function inputDefault() {
-  inputMin.value = "05";
-  inputSec.value = "00";
-}
+let compteur = null;
 
 class Display {
-  constructor(stop, start, pause) {
-    this.stop = stop;
-    this.start = start;
-    this.pause = pause;
+  constructor(state) {
+    this.inputMin = document.querySelector(state.inputMin);
+    this.inputSec = document.querySelector(state.inputSec);
+    this.setInputDefault()
+    this.stop = document.querySelector(state.btnStop);
+    this.start = document.querySelector(state.btnStart);
+    this.pause = document.querySelector(state.btnPause);
+  }
+
+  optionDisplay(state) {
+    this.stop.style.display = state.stop
+    this.start.style.display = state.start
+    this.pause.style.display = state.pause
+  }
+
+  showBtnStart(toto) {
+    this.start.style.display = toto ? "block" : "none"
+  }
+
+  showBtnStop(toto) {
+    this.stop.style.display = toto ? "block" : "none"
+  }
+  
+  showBtnPause(toto) {
+    this.pause.style.display = toto ? "block" : "none"
+  }
+
+//Si true bloques les inputs, si false ils sont débloqués
+  inputReadOnly(lol) {
+    this.inputMin.readOnly = lol;
+    this.inputSec.readOnly = lol;
+  }
+
+  setInputDefault() {
+    this.inputMin.value = inputDefault.min;
+    this.inputSec.value = inputDefault.sec;
   }
 }
 
-function displayButton() {
-  document.querySelector("#stop").style.display = diplayOff.stop;
-  document.querySelector("#start").style.display = diplayOff.start;
-  document.querySelector("#pause").style.display = diplayOff.pause;
+let displayEl = {
+  inputMin : "#inputMin",
+  inputSec : "#inputSec",
+  btnStop : "#stop",
+  btnStart : "#start",
+  btnPause : "#pause"
 }
 
-diplayOff = new Display("none", "block", "none");
-displayButton();
+let displayOpt = {
+  stop : "none",
+  start : "block",
+  pause : "none"
+}
 
-const start = document.getElementById("start");
-start.addEventListener("click", function () {
-  diplayOff = new Display("block", "none", "block");
-  displayButton();
-  inputOff();
-  startTimer();
-});
+let display = new Display(displayEl)
+display.optionDisplay(displayOpt)
+
+function updateTimer() {
+  if (timerSec > 0) {
+    let minutes = Math.floor(timerSec / 60);
+    let secondes = timerSec % 60;
+    
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    secondes = secondes < 10 ? "0" + secondes : secondes;
+    
+    display.inputMin.value = minutes;
+    display.inputSec.value = secondes;
+    
+    timerSec--;
+
+  } else {
+    const alarme = new Audio("ah.mp3");
+    alarme.volume = 0.7;
+    alarme.play();
+    stopTimer()
+  }
+}
 
 function startTimer() {
-  const inputMin = document.getElementById("inputMin");
-  let min = parseInt(inputMin.value);
-
-  const inputSec = document.getElementById("inputSec");
-  let sec = parseInt(inputSec.value);
-
+  display.inputReadOnly(true);
+  display.showBtnStop(true);
+  display.showBtnStart(false);
+  display.showBtnPause(true);
+  
+  let min = parseInt(display.inputMin.value);
+  let sec = parseInt(display.inputSec.value);
+  
   let convMin = min * 60;
-  let timerSec = convMin + sec;
-
+  timerSec = convMin + sec;
+  
   timerSec--;
 
-  const compteur = setInterval(function () {
-    if (timerSec > 0) {
-      let minutes = Math.floor(timerSec / 60);
-      let secondes = timerSec % 60;
-
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      secondes = secondes < 10 ? "0" + secondes : secondes;
-
-      inputMin.value = minutes;
-      inputSec.value = secondes;
-
-      timerSec--;
-    } else {
-      const alarme = new Audio("ah.mp3");
-      alarme.volume = 0.7;
-      alarme.play();
-
-      inputDefault();
-
-      diplayOff = new Display("none", "block", "none");
-      displayButton();
-
-      inputOn();
-
-      stopInterval(compteur);
-    }
-  }, 1000);
-
-  const stop = document.getElementById("stop");
-  stop.addEventListener("click", function () {
-    diplayOff = new Display("none", "block", "none");
-    displayButton();
-    inputDefault();
-    inputOn();
-    stopInterval();
-  });
-
-  const pause = document.getElementById("pause");
-  pause.addEventListener("click", function () {
-    stopInterval();
-    inputOff();
-
-    diplayOff = new Display("block", "block", "none");
-    displayButton();
-  });
-
-  function stopInterval() {
-    clearInterval(compteur);
-  }
+  compteur = setInterval(updateTimer, 1000);
 }
 
-// function setTimer() {
-//   if (isNaN(sec) || isNaN(min)) {
-//     alert("toto");
-//   } else {}
-// }
+
+function stopTimer() {
+  clearInterval(compteur);
+  display.setInputDefault();
+  display.inputReadOnly(false);
+  display.showBtnStop(false);
+  display.showBtnStart(true);
+  display.showBtnPause(false);
+}
+
+function pauseTimer() {
+  clearInterval(compteur);
+  display.inputReadOnly(false);
+  display.showBtnStop(true);
+  display.showBtnStart(true);
+  display.showBtnPause(false);
+}
+
+display.stop.addEventListener("click", stopTimer);
+display.start.addEventListener("click", startTimer);
+display.pause.addEventListener("click", pauseTimer);
+
 
 /* setTimeout(function () {
   for (let i = 0; i < 2; i++) {
